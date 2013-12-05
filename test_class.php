e53453eef779d953a4a20ec6754d55a9aea0e100
khrome <abbey@khrome.net>
2013-12-04 23:34:58 -0800

    added mongo tests

--- test_class.php
+++ test_class.php
@@ -1,9 +1,17 @@
 <?php
 class User extends Protolus_Data{
     function __construct(){
-        $this->datasource = 'maindatabase';
+        $this->datasource = 'mysqldatabase';
         parent::__construct();
         $this->type = 'users';
     }
 }
 Protolus_Data::register(User);
+class Uzer extends Protolus_Data{
+    function __construct(){
+        $this->datasource = 'mongodatabase';
+        parent::__construct();
+        $this->type = 'users';
+    }
+}
+Protolus_Data::register(Uzer);
